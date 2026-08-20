a36ffe7aef619455c8afc472681f2ce14ab25666
Nik-Nitro <dravkostin2@mail.ru>
2026-08-21 01:45:13 +0300

    Update __init__.py

--- p4a-recipes/hostpython3/__init__.py
+++ p4a-recipes/hostpython3/__init__.py
@@ -1,7 +1,6 @@
 from pythonforandroid.recipe import Recipe
 from pythonforandroid.logger import info
 import os
-import sh
 import sys
 
 class HostPython3Recipe(Recipe):
@@ -22,45 +21,16 @@ class HostPython3Recipe(Recipe):
 
     def build_arch(self, arch):
         info('HostPython3: using system python, skipping build')
-        pass
+        return
 
     def get_path_to_python(self):
-        # Пытаемся найти Python в системе
-        for path in ['/usr/bin/python3.11', '/usr/bin/python3', '/home/user/.venv/bin/python3']:
-            if os.path.exists(path):
-                info(f'HostPython3: found python at {path}')
-                return path
-        # Если не нашли - используем sys.executable
-        info(f'HostPython3: using sys.executable: {sys.executable}')
+        # Используем Python 3.11 из контейнера
+        python_path = '/usr/local/bin/python3.11'
+        if os.path.exists(python_path):
+            info(f'HostPython3: found python at {python_path}')
+            return python_path
+        # fallback
         return sys.executable
-
-    @property
-    def python_exe(self):
-        return self.get_path_to_python()
-
-    @property
-    def local_bin(self):
-        return '/usr/local/bin'
-
-    @property
-    def site_bin(self):
-        return '/usr/local/bin'
-
-    @property
-    def site_dir(self):
-        import sys
-        for path in sys.path:
-            if 'site-packages' in path:
-                return path
-        return '/usr/local/lib/python3.11/site-packages'
-
-    @property
-    def pip(self):
-        return sh.Command(self.get_path_to_python(), '-m', 'pip')
-
-    @property
-    def python(self):
-        return sh.Command(self.get_path_to_python())
 
     def get_build_dir(self, arch_name):
         return os.path.join(self.ctx.build_dir, 'other_builds', 'hostpython3', arch_name)
