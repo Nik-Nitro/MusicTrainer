dc9a3a20051e30bc9218820644217f66e96fcae2
Nik-Nitro <dravkostin2@mail.ru>
2026-08-20 21:07:24 +0300

    Update kivy_setup_patch.py

--- kivy_setup_patch.py
+++ kivy_setup_patch.py
@@ -1,33 +1,10 @@
 import os
-import sys
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 
-# Переопределяем поиск библиотек
+# Отключаем X11
 os.environ['USE_X11'] = '0'
 os.environ['KIVY_GL_BACKEND'] = 'sdl2'
 os.environ['KIVY_NO_X11'] = '1'
-os.environ['KIVY_USE_SDL2'] = '1'
+os.environ['KIVY_USE_X11'] = '0'
 
-class CustomBuildExt(build_ext):
-    def build_extensions(self):
-        # Удаляем все ссылки на X11 из линковки
-        for ext in self.extensions:
-            if hasattr(ext, 'libraries'):
-                if 'X11' in ext.libraries:
-                    ext.libraries.remove('X11')
-                if 'x11' in ext.libraries:
-                    ext.libraries.remove('x11')
-            if hasattr(ext, 'extra_link_args'):
-                ext.extra_link_args = [
-                    arg for arg in ext.extra_link_args 
-                    if 'X11' not in arg and 'x11' not in arg
-                ]
-        super().build_extensions()
-
-# Подменяем команду build_ext
-setup(
-    cmdclass={
-        'build_ext': CustomBuildExt,
-    },
-)
+from setuptools import setup
+setup()
