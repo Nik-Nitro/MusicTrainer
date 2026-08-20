ec0f949d56d2df70eac1710bc44734c2678c868c
Nik-Nitro <dravkostin2@mail.ru>
2026-08-20 11:47:22 +0300

    Update __init__.py

--- p4a-recipes/kivy/__init__.py
+++ p4a-recipes/kivy/__init__.py
@@ -1,43 +1,42 @@
-from pythonforandroid.recipe import CythonRecipe
+from pythonforandroid.recipe import MesonRecipe
 from pythonforandroid.logger import info
 import os
-import shutil
 
-class KivyRecipe(CythonRecipe):
+class KivyRecipe(MesonRecipe):
     name = 'kivy'
     version = '2.1.0'
     url = 'https://github.com/kivy/kivy/archive/{version}.zip'
     depends = ['python3', 'sdl2', 'pygame']
 
-    def prebuild_arch(self, arch):
-        super().prebuild_arch(arch)
-        
-        # Удаляем все X11-файлы
-        build_dir = self.get_build_dir(arch.arch)
-        kivy_dir = os.path.join(build_dir, 'kivy')
-        
-        # Удаляем все файлы с x11 в названии
-        for root, dirs, files in os.walk(kivy_dir):
-            for f in files:
-                if 'x11' in f.lower():
-                    file_path = os.path.join(root, f)
-                    info(f'Removing X11 file: {file_path}')
-                    os.remove(file_path)
-        
-        # Удаляем папку window_x11 если есть
-        window_dir = os.path.join(kivy_dir, 'core', 'window')
-        if os.path.exists(window_dir):
-            for f in os.listdir(window_dir):
-                if 'x11' in f.lower():
-                    file_path = os.path.join(window_dir, f)
-                    if os.path.isfile(file_path):
-                        info(f'Removing X11 file: {f}')
-                        os.remove(file_path)
-
     def get_recipe_env(self, arch):
         env = super().get_recipe_env(arch)
+        
+        # Отключаем X11
         env['USE_X11'] = '0'
         env['KIVY_GL_BACKEND'] = 'sdl2'
+        
+        # Добавляем флаги для кросс-компиляции
+        ndk_sysroot = self.ctx.ndk.sysroot
+        env['CFLAGS'] = env.get('CFLAGS', '') + f' -I{ndk_sysroot}/usr/include'
+        env['CXXFLAGS'] = env.get('CXXFLAGS', '') + f' -I{ndk_sysroot}/usr/include'
+        
+        # Добавляем путь к Python include
+        python_include = os.path.join(
+            self.ctx.get_python_install_dir(arch.arch),
+            'android-build', 'android-root', 'include', 'python3.11'
+        )
+        env['CFLAGS'] = env.get('CFLAGS', '') + f' -I{python_include}'
+        env['CXXFLAGS'] = env.get('CXXFLAGS', '') + f' -I{python_include}'
+        
         return env
+
+    def get_meson_options(self, arch):
+        """Возвращает опции для Meson"""
+        return [
+            '-Duse_x11=disabled',
+            '-Duse_sdl2=enabled',
+            '-Dgl_backend=sdl2',
+            f'-Dpython_include_dir={self.ctx.get_python_install_dir(arch.arch)}/android-build/android-root/include/python3.11',
+        ]
 
 recipe = KivyRecipe()
