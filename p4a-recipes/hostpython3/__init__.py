3c5715078285170fba609a6e47984b51af7b5df0
Nik-Nitro <dravkostin2@mail.ru>
2026-08-21 02:12:07 +0300

    Update __init__.py

--- p4a-recipes/hostpython3/__init__.py
+++ p4a-recipes/hostpython3/__init__.py
@@ -2,7 +2,6 @@ from pythonforandroid.recipe import Recipe
 from pythonforandroid.logger import info
 import os
 import sys
-import sh
 
 class HostPython3Recipe(Recipe):
     name = 'hostpython3'
@@ -29,7 +28,6 @@ class HostPython3Recipe(Recipe):
         if os.path.exists(python_path):
             info(f'HostPython3: found python at {python_path}')
             return python_path
-        info(f'HostPython3: using fallback {sys.executable}')
         return sys.executable
 
     @property
@@ -46,19 +44,19 @@ class HostPython3Recipe(Recipe):
 
     @property
     def site_dir(self):
-        import sys
         for path in sys.path:
             if 'site-packages' in path:
                 return path
         return '/usr/local/lib/python3.11/site-packages'
 
+    # Упрощаем - просто строки
     @property
     def pip(self):
-        return sh.Command(self.get_path_to_python(), '-m', 'pip')
+        return '/usr/local/bin/pip3.11'
 
     @property
     def python(self):
-        return sh.Command(self.get_path_to_python())
+        return self.get_path_to_python()
 
     def get_build_dir(self, arch_name):
         return os.path.join(self.ctx.build_dir, 'other_builds', 'hostpython3', arch_name)
