4ef6bea571830dd87e7e20ef43f588d3828b5a63
Nik-Nitro <dravkostin2@mail.ru>
2026-08-21 00:50:13 +0300

    Update __init__.py

--- p4a-recipes/kivy/__init__.py
+++ p4a-recipes/kivy/__init__.py
@@ -1,6 +1,7 @@
 from pythonforandroid.recipe import PythonRecipe
 import os
 import re
+import glob
 
 class KivyRecipe(PythonRecipe):
     version = '2.1.0'
@@ -18,6 +19,32 @@ class KivyRecipe(PythonRecipe):
     def build_arch(self, arch):
         build_dir = self.get_build_dir(arch.arch)
         
+        # Патчим все .py, .pyx, .pxd файлы от X11
+        for root, dirs, files in os.walk(build_dir):
+            for file in files:
+                if file.endswith(('.py', '.pyx', '.pxd')):
+                    path = os.path.join(root, file)
+                    try:
+                        with open(path, 'r') as f:
+                            content = f.read()
+                        content = re.sub(r'USE_X11\s*=\s*[10]', 'USE_X11 = 0', content)
+                        content = re.sub(r'KIVY_USE_X11\s*=\s*[10]', 'KIVY_USE_X11 = 0', content)
+                        content = re.sub(r'if\s+USE_X11\s*:', 'if False:', content)
+                        content = re.sub(r'elif\s+USE_X11\s*:', 'elif False:', content)
+                        with open(path, 'w') as f:
+                            f.write(content)
+                    except:
+                        pass
+        
+        # Удаляем файлы X11
+        for pattern in ['**/window_x11.py', '**/window_x11.pyx', '**/window_x11.pxd']:
+            for path in glob.glob(f'{build_dir}/{pattern}', recursive=True):
+                try:
+                    os.remove(path)
+                    print(f"Removed X11 file: {path}")
+                except:
+                    pass
+        
         # Патчим setup.py
         setup_path = f'{build_dir}/setup.py'
         if os.path.exists(setup_path):
@@ -30,8 +57,11 @@ os.environ['KIVY_GL_BACKEND'] = 'sdl2'
 os.environ['KIVY_NO_X11'] = '1'
 os.environ['KIVY_USE_X11'] = '0'
 '''
+            content = patch + content
+            content = re.sub(r'use_x11\s*=\s*True', 'use_x11 = False', content)
             with open(setup_path, 'w') as f:
-                f.write(patch + content)
+                f.write(content)
+            print(f"Patched setup.py at: {setup_path}")
         
         super().build_arch(arch)
 
