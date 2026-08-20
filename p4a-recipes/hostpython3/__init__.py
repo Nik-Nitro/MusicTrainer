4bbe80730e8fbe8d808f6a7d589c5dec4f1107da
Nik-Nitro <dravkostin2@mail.ru>
2026-08-20 09:42:49 +0300

    Update __init__.py

--- p4a-recipes/hostpython3/__init__.py
+++ p4a-recipes/hostpython3/__init__.py
@@ -1,6 +1,7 @@
 from pythonforandroid.recipe import Recipe
 from pythonforandroid.logger import info
 import os
+import sh
 
 class HostPython3Recipe(Recipe):
     name = 'hostpython3'
@@ -15,19 +16,19 @@ class HostPython3Recipe(Recipe):
 
     def get_path_to_python(self):
         # Для GitHub Actions путь к Python 3.11
-        return '/opt/hostedtoolcache/Python/3.11.16/x64/bin/python3'
+        return '/usr/bin/python3'
 
     @property
     def python_exe(self):
-        return '/opt/hostedtoolcache/Python/3.11.16/x64/bin/python3'
+        return '/usr/bin/python3'
 
     @property
     def local_bin(self):
-        return '/opt/hostedtoolcache/Python/3.11.16/x64/bin'
+        return '/usr/bin'
 
     @property
     def site_bin(self):
-        return '/opt/hostedtoolcache/Python/3.11.16/x64/bin'
+        return '/usr/bin'
 
     @property
     def site_dir(self):
@@ -35,17 +36,15 @@ class HostPython3Recipe(Recipe):
         for path in sys.path:
             if 'site-packages' in path:
                 return path
-        return '/opt/hostedtoolcache/Python/3.11.16/x64/lib/python3.11/site-packages'
+        return '/usr/lib/python3.11/site-packages'
 
     @property
     def pip(self):
-        import sh
-        return sh.Command('/opt/hostedtoolcache/Python/3.11.16/x64/bin/pip3')
+        return sh.Command('/usr/bin/pip3')
 
     @property
     def python(self):
-        import sh
-        return sh.Command('/opt/hostedtoolcache/Python/3.11.16/x64/bin/python3')
+        return sh.Command('/usr/bin/python3')
 
     def get_build_dir(self, arch_name):
         return os.path.join(self.ctx.build_dir, 'other_builds', 'hostpython3', arch_name)
