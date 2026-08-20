20614283ed4992b64c93656a6ab710f8c62a79b1
Nik-Nitro <dravkostin2@mail.ru>
2026-08-21 01:58:29 +0300

    Update __init__.py

--- p4a-recipes/hostpython3/__init__.py
+++ p4a-recipes/hostpython3/__init__.py
@@ -2,6 +2,7 @@ from pythonforandroid.recipe import Recipe
 from pythonforandroid.logger import info
 import os
 import sys
+import sh
 
 class HostPython3Recipe(Recipe):
     name = 'hostpython3'
@@ -24,18 +25,40 @@ class HostPython3Recipe(Recipe):
         return
 
     def get_path_to_python(self):
-        """Возвращает путь к Python 3.11"""
         python_path = '/usr/local/bin/python3.11'
         if os.path.exists(python_path):
             info(f'HostPython3: found python at {python_path}')
             return python_path
-        # fallback
+        info(f'HostPython3: using fallback {sys.executable}')
         return sys.executable
 
-    # ВАЖНО: Добавляем property python_exe
     @property
     def python_exe(self):
         return self.get_path_to_python()
+
+    @property
+    def local_bin(self):
+        return '/usr/local/bin'
+
+    @property
+    def site_bin(self):
+        return '/usr/local/bin'
+
+    @property
+    def site_dir(self):
+        import sys
+        for path in sys.path:
+            if 'site-packages' in path:
+                return path
+        return '/usr/local/lib/python3.11/site-packages'
+
+    @property
+    def pip(self):
+        return sh.Command(self.get_path_to_python(), '-m', 'pip')
+
+    @property
+    def python(self):
+        return sh.Command(self.get_path_to_python())
 
     def get_build_dir(self, arch_name):
         return os.path.join(self.ctx.build_dir, 'other_builds', 'hostpython3', arch_name)
