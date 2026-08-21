c8d4f4e21d3ea051341e27de07cb37c8b1b4e61b
Nik-Nitro <dravkostin2@mail.ru>
2026-08-21 17:00:34 +0300

    Update __init__.py

--- p4a-recipes/kivy/__init__.py
+++ p4a-recipes/kivy/__init__.py
@@ -9,15 +9,28 @@ class KivyRecipe(PythonRecipe):
     
     def get_recipe_env(self, arch):
         env = super().get_recipe_env(arch)
+        
+        # Отключаем X11
         env['USE_X11'] = '0'
         env['KIVY_GL_BACKEND'] = 'sdl2'
         env['KIVY_NO_X11'] = '1'
         env['KIVY_USE_X11'] = '0'
-        env['CFLAGS'] = env.get('CFLAGS', '') + ' -DKIVY_NO_X11 -DUSE_X11=0'
+        
+        # Включаем OpenGL ES 2.0
+        env['KIVY_GLES'] = '1'
+        env['KIVY_GLES2'] = '1'
+        env['USE_GLES2'] = '1'
+        
+        # Добавляем флаги компиляции
+        cflags = env.get('CFLAGS', '')
+        cflags += ' -DKIVY_NO_X11 -DUSE_X11=0 -DKIVY_GLES2=1 -DKIVY_GLES=1'
+        env['CFLAGS'] = cflags
+        
         return env
     
     def build_arch(self, arch):
         build_dir = self.get_build_dir(arch.arch)
+        print(f"=== KIVY: Building for {arch} in {build_dir} ===")
         
         # Патчим все .py, .pyx, .pxd файлы от X11
         for root, dirs, files in os.walk(build_dir):
@@ -33,8 +46,8 @@ class KivyRecipe(PythonRecipe):
                         content = re.sub(r'elif\s+USE_X11\s*:', 'elif False:', content)
                         with open(path, 'w') as f:
                             f.write(content)
-                    except:
-                        pass
+                    except Exception as e:
+                        print(f"Could not patch {path}: {e}")
         
         # Удаляем файлы X11
         for pattern in ['**/window_x11.py', '**/window_x11.pyx', '**/window_x11.pxd']:
@@ -42,8 +55,8 @@ class KivyRecipe(PythonRecipe):
                 try:
                     os.remove(path)
                     print(f"Removed X11 file: {path}")
-                except:
-                    pass
+                except Exception as e:
+                    print(f"Could not remove {path}: {e}")
         
         # Патчим setup.py
         setup_path = f'{build_dir}/setup.py'
@@ -56,6 +69,9 @@ os.environ['USE_X11'] = '0'
 os.environ['KIVY_GL_BACKEND'] = 'sdl2'
 os.environ['KIVY_NO_X11'] = '1'
 os.environ['KIVY_USE_X11'] = '0'
+os.environ['KIVY_GLES2'] = '1'
+os.environ['KIVY_GLES'] = '1'
+os.environ['USE_GLES2'] = '1'
 '''
             content = patch + content
             content = re.sub(r'use_x11\s*=\s*True', 'use_x11 = False', content)
@@ -63,6 +79,8 @@ os.environ['KIVY_USE_X11'] = '0'
                 f.write(content)
             print(f"Patched setup.py at: {setup_path}")
         
+        # Запускаем стандартную сборку
         super().build_arch(arch)
+        print(f"=== KIVY: Build completed for {arch} ===")
 
 recipe = KivyRecipe()
