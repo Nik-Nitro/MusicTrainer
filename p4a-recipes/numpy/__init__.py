eb34a59edaaacf2ccdde15f9f447daf069d12f59
Nik-Nitro <dravkostin2@mail.ru>
2026-08-22 19:10:17 +0300

    Create __init__.py

---
+++ p4a-recipes/numpy/__init__.py
@@ -0,0 +1,29 @@
+from pythonforandroid.recipe import PythonRecipe
+from pythonforandroid.logger import info
+import os
+
+class NumpyRecipe(PythonRecipe):
+    version = '1.26.4'
+    url = 'https://pypi.org/packages/source/n/numpy/numpy-{version}.zip'
+    depends = ['python3', 'hostpython3']
+    
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        
+        # Отключаем BLAS/LAPACK для упрощения сборки на Android
+        env['NPY_BLAS_ORDER'] = ''
+        env['NPY_LAPACK_ORDER'] = ''
+        env['ATLAS'] = 'None'
+        
+        # Флаги для кросс-компиляции
+        env['CFLAGS'] = env.get('CFLAGS', '') + ' -O2 -fPIC'
+        
+        info(f'NumpyRecipe: Setting up environment for {arch}')
+        return env
+    
+    def build_arch(self, arch):
+        info(f'NumpyRecipe: Building for {arch}')
+        super().build_arch(arch)
+        info(f'NumpyRecipe: Build completed for {arch}')
+
+recipe = NumpyRecipe()
